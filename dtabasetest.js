e181920af84a5c675bf52991cc8f342c309a0fc1
Sandesh Rai <pumasandeshrai@gmail.com>
2026-02-11 14:02:02 +0545

    updated

---
+++ dtabasetest.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+// Use your actual string from Atlas here
+const dbURI = "mongodb+srv://rai:parmilarai@cluster0.xygaeln.mongodb.net/Survey?appName=Cluster0";
+
+mongoose.connect(dbURI)
+  .then(() => console.log("✅ MongoDB Connected! Now UserModel.find() will work."))
+  .catch(err => console.log("❌ Connection Error:", err));
